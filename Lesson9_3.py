942193db34911a54ce5c8a397c95f007792ec284
kassacs <48911709+kassacs@users.noreply.github.com>
2020-04-17 16:58:46 +0500

    Create Lesson9_3.py

---
+++ Lesson9_3.py
@@ -0,0 +1,28 @@
+from random import choice
+from pprint import pprint
+pars={}
+def answer(question):
+    def Compare(S1,S2):
+        ngrams = [S1[i:i+3] for i in range(len(S1))]
+        n=0
+        for j in ngrams:
+            n += S2.count(j)
+        return (n/max(len(S1),len(S2)))
+    if len(pars) >0:
+        for key in pars.keys():
+           print (Compare(key,question))
+           if Compare(key,question)>0.5: 
+             return key, pars[key]
+             break
+           pars.update([(question,choice(['yes', 'no']))])
+           return question, pars[question]
+    else:
+        pars.update([(question,choice(['yes', 'no']))])
+        return question, pars[question]
+questions= ["It's you?","Are you Article Intelligent?",
+           "Do you like Arduino?",
+           "Do you eat Mushrooms?",
+           "It's youuu?"
+            ]
+for question in questions:
+    pprint(answer(question))
