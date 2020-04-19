54cfadc95aa84c49358c436d4b7083c47a0df045
kassacs <48911709+kassacs@users.noreply.github.com>
2020-04-19 16:57:17 +0500

    Update Lesson9_3.py
    PEP8

--- Lesson9_3.py
+++ Lesson9_3.py
@@ -1,28 +1,27 @@
-# Задание: гадалка с памятью и слегка переформулированными вопросами
 from random import choice
 from pprint import pprint
-pars={}
+pars = {}
 def answer(question):
-    def Compare(S1,S2):
-        ngrams = [S1[i:i+3] for i in range(len(S1))]
-        n=0
+    def compare(S1,S2):
+        ngrams = [S1[i:i + 3] for i in range(len(S1))]
+        n = 0
         for j in ngrams:
             n += S2.count(j)
-        return (n/max(len(S1),len(S2)))
-    if len(pars) >0:
+        return (n / max(len(S1), len(S2)))
+    if len(pars) > 0:
         for key in pars.keys():
-           print (Compare(key,question))
-           if Compare(key,question)>0.5: 
-             return key, pars[key]
+            print (Compare(key,question))
+            if Compare(key,question) > 0.5: 
+                return key, pars[key]
     pars.update([(question,choice(['yes', 'no']))])
     return question, pars[question]
     else:
         pars.update([(question,choice(['yes', 'no']))])
         return question, pars[question]
-questions= ["It's you?","Are you Article Intelligent?",
-           "Do you like Arduino?",
-           "Do you eat Mushrooms?",
-           "It's youuu?"
-            ]
+questions = ["It's you?","Are you Article Intelligent?",
+            "Do you like Arduino?",
+            "Do you eat Mushrooms?",
+            "It's youuu?"
+             ]
 for question in questions:
     pprint(answer(question))
