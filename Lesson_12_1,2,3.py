b2e0918585ced70240096ac781d610e4fa9dc30e
kassacs <48911709+kassacs@users.noreply.github.com>
2020-04-22 16:53:16 +0500

    Update Lesson_12_1,2,3.py

--- Lesson_12_1,2,3.py
+++ Lesson_12_1,2,3.py
@@ -1,1 +1,11 @@
-
+class Tolkien_subjects(): # выбран класс обобщающий логически всех персонажей, имя выбано в соответствии с рекомендациями PEP8
+    def __init__(self,rasa,name,family,height,weight): #  в качестве методов выбраны характеристики персонажей
+        self.rasa = rasa
+        self.name = name
+        self.family = family
+        self.height = height
+        self.weight = weight
+    def go(self): # Выбран глагол в соответствии с рекомeнlациями PEP8 
+        return "going"
+    def stop(self): # Выбран глагол в соответствии с рекомeнlациями PEP8 
+        return "stopping"
